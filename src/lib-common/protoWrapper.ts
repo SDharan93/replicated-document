2df41e6ef3eb8c12450f69f2dc5e2c9487b7f03d
shane.dharan <shanedharan@gmail.com>
2019-02-18 23:19:19 -0500

    Create a protobuf wrapper class
    This class will be used to encapsulte all protobufs in the code. Since
    protobufs do not have the ability to include logical methods, we will
    pass these wrappers around instead of the proto instances.

---
+++ src/lib-common/protoWrapper.ts
@@ -0,0 +1,10 @@
+/**
+ * This class wraps proto buffers and adds a layer of logic to each message type. Proto buffers should not be directly
+ * used in code. By using wrappers we can swap message definitions and avoid large refactors.
+ */
+abstract class ProtoWrapper <Proto> {
+    // Returns the underlying proto for the wrapper.
+    public abstract getProto(): Proto;
+}
+
+export { ProtoWrapper };
