1a0d6dedcf7e6cd8052d40be8a888af09201dcc4
Shane Dharan <shanedharan@gmail.com>
2019-02-27 21:19:57 -0500

    Add functionality for document modifications (#6)
    * Remove document lines as it is an optimization we do not need
    
    * Add logic to compare two replica characters
    
    * Create tests for comparing two replica characters
    
    * Remove stub test for position identifier
    
    * Add methods to delete characters from the document.
    
    * Add unit tests for deletion characters from the document.
    
    * Fix linting issues.
    
    - Removed lint check for magic numbers
    - Removed lint check for comment case-sensitivity
    - These lint errors were making the code harder to read and more bloaty.
      We will have to catch these things where it makes sense in code
      review.
    
    * Add the ability to insert into the document
    
    - This method will use binary search and look for the appropriate
      place to insert. This insert will likely be used for remote sync.

---
+++ src/test/lib-crdt/documentWrapper.test.ts
@@ -0,0 +1,168 @@
+import { DocumentWrapper } from "../../lib-crdt/documentWrapper";
+import { ReplicaCharacterWrapper } from "../../lib-crdt/replicaCharacterWrapper";
+import { document } from "../../lib-proto/document";
+import Document = document.data.Document;
+import PositionIdentifier = document.data.PositionIdentifier;
+import ReplicaCharacter = document.data.ReplicaCharacter;
+
+describe("DocumentWrapper", () => {
+    let document: Document;
+    let subject: DocumentWrapper;
+    let characters: ReplicaCharacter[];
+
+    beforeEach(() => {
+        characters = [
+            ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: 0, siteId: "b67d409a"})
+                ]
+            }),
+            ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: 1, siteId: "b67d409a"})
+                ]
+            }),
+            ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: 2, siteId: "b67d409a"})
+                ]
+            }),
+            ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: 3, siteId: "b67d409a"})
+                ]
+            }),
+            ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: 4, siteId: "b67d409a"})
+                ]
+            })
+        ];
+    });
+
+    describe("deleteCharacter", () => {
+        beforeEach(() => {
+            document = Document.create({
+                characters: characters
+            });
+        });
+
+        test("should delete middle character", () => {
+            const removedChar: ReplicaCharacterWrapper = new ReplicaCharacterWrapper(characters[2]);
+            subject = new DocumentWrapper(document);
+
+            const success: boolean = subject.deleteCharacter(removedChar);
+
+            expect(success).toEqual(true);
+            expect(subject.replicaCharacters.length).toEqual(4);
+            expect(subject.replicaCharacters).not.toContain(removedChar);
+        });
+
+        test("should delete first character", () => {
+            const removedChar: ReplicaCharacterWrapper = new ReplicaCharacterWrapper(characters[0]);
+            subject = new DocumentWrapper(document);
+
+            const success: boolean = subject.deleteCharacter(removedChar);
+
+            expect(success).toEqual(true);
+            expect(subject.replicaCharacters).toHaveLength(4);
+            expect(subject.replicaCharacters).not.toContain(removedChar);
+        });
+
+        test("should delete last character", () => {
+            const lastCharIndex: number = characters.length - 1;
+            const removedChar: ReplicaCharacterWrapper = new ReplicaCharacterWrapper(characters[lastCharIndex]);
+            subject = new DocumentWrapper(document);
+
+            const success: boolean = subject.deleteCharacter(removedChar);
+
+            expect(success).toEqual(true);
+            expect(subject.replicaCharacters).toHaveLength(4);
+            expect(subject.replicaCharacters).not.toContain(removedChar);
+        });
+
+        test("should report unsuccessful when large character is not found", () => {
+            const nonExistingChar: ReplicaCharacter = ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: 100, siteId: "b67d409a"})
+                ]
+            });
+            const removedChar: ReplicaCharacterWrapper = new ReplicaCharacterWrapper(nonExistingChar);
+            subject = new DocumentWrapper(document);
+
+            const success: boolean = subject.deleteCharacter(removedChar);
+
+            expect(success).toEqual(false);
+            expect(subject.replicaCharacters).toHaveLength(5);
+        });
+
+        test("should report unsuccessful when small character is not found", () => {
+            const nonExistingChar: ReplicaCharacter = ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: -1, siteId: "b67d409a"})
+                ]
+            });
+            const removedChar: ReplicaCharacterWrapper = new ReplicaCharacterWrapper(nonExistingChar);
+            subject = new DocumentWrapper(document);
+
+            const success: boolean = subject.deleteCharacter(removedChar);
+
+            expect(success).toEqual(false);
+            expect(subject.replicaCharacters).toHaveLength(5);
+        });
+    });
+
+    describe("insertCharacter", () => {
+        beforeEach(() => {
+            document = Document.create({
+                characters: characters
+            });
+        });
+
+        test("should insert character to head of document", () => {
+            const insertCharacter: ReplicaCharacterWrapper = new ReplicaCharacterWrapper(ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: -1, siteId: "b67d409a"})
+                ]
+            }));
+            subject = new DocumentWrapper(document);
+
+            const success: boolean = subject.insertCharacter(insertCharacter);
+
+            expect(success).toEqual(true);
+            expect(subject.replicaCharacters).toHaveLength(characters.length + 1);
+            expect(subject.replicaCharacters[0]).toEqual(insertCharacter);
+        });
+
+        test("should insert character to tail of document", () => {
+            const insertCharacter: ReplicaCharacterWrapper = new ReplicaCharacterWrapper(ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: 100, siteId: "b67d409a"})
+                ]
+            }));
+            subject = new DocumentWrapper(document);
+
+            const success: boolean = subject.insertCharacter(insertCharacter);
+
+            expect(success).toEqual(true);
+            expect(subject.replicaCharacters).toHaveLength(characters.length + 1);
+            expect(subject.replicaCharacters[5]).toEqual(insertCharacter);
+        });
+
+        test("should insert character to midde of document", () => {
+            const insertCharacter: ReplicaCharacterWrapper = new ReplicaCharacterWrapper(ReplicaCharacter.create({
+                positions: [
+                    PositionIdentifier.create({digit: 3, siteId: "b67d409a"}),
+                    PositionIdentifier.create({digit: 5, siteId: "b67d409a"})
+                ]
+            }));
+            subject = new DocumentWrapper(document);
+
+            const success: boolean = subject.insertCharacter(insertCharacter);
+
+            expect(success).toEqual(true);
+            expect(subject.replicaCharacters).toHaveLength(characters.length + 1);
+            expect(subject.replicaCharacters[4]).toEqual(insertCharacter);
+        });
+    });
+});
--- src/test/lib-crdt/replicaCharacterWrapper.test.ts
+++ src/test/lib-crdt/replicaCharacterWrapper.test.ts
@@ -1,6 +1,7 @@
 import { ReplicaCharacterWrapper } from "../../lib-crdt/replicaCharacterWrapper";
 import { document } from "../../lib-proto/document";
 import ReplicaCharacter = document.data.ReplicaCharacter;
+import PositionIdentifier = document.data.PositionIdentifier;
 
 describe("ReplicaCharacterWrapper", () => {
     let replicaCharacter: ReplicaCharacter;
@@ -10,14 +11,115 @@ describe("ReplicaCharacterWrapper", () => {
         replicaCharacter = ReplicaCharacter.create();
     });
 
-    // This is just a simple test to make sure Jest is working.
-    // TODO: Remove this test once we have real logic.
-    test("should return value", () => {
-        replicaCharacter.value = "A";
-        subject = new ReplicaCharacterWrapper(replicaCharacter);
+    describe("compare", () => {
+        let comparable: ReplicaCharacter;
 
-        const value: string = subject.value;
+        beforeEach(() => {
+            comparable = ReplicaCharacter.create();
+        });
 
-        expect(value).toEqual(replicaCharacter.value);
+        test("should be greater than with no prefix", () => {
+            replicaCharacter.positions = [
+                PositionIdentifier.create({digit: 2})
+            ];
+            comparable.positions = [
+                PositionIdentifier.create({digit: 1})
+            ];
+            subject = new ReplicaCharacterWrapper(replicaCharacter);
+
+            const digit: number = subject.compare(new ReplicaCharacterWrapper(comparable));
+
+            expect(digit).toEqual(1);
+        });
+
+        test("should be less than with no prefix", () => {
+            replicaCharacter.positions = [
+                PositionIdentifier.create({digit: 1})
+            ];
+            comparable.positions = [
+                PositionIdentifier.create({digit: 2})
+            ];
+            subject = new ReplicaCharacterWrapper(replicaCharacter);
+
+            const digit: number = subject.compare(new ReplicaCharacterWrapper(comparable));
+
+            expect(digit).toEqual(-1);
+        });
+
+        test("should be greater than with common prefix", () => {
+            replicaCharacter.positions = [
+                PositionIdentifier.create({digit: 1}),
+                PositionIdentifier.create({digit: 2}),
+                PositionIdentifier.create({digit: 3})
+            ];
+            comparable.positions = [
+                PositionIdentifier.create({digit: 1}),
+                PositionIdentifier.create({digit: 2})
+            ];
+            subject = new ReplicaCharacterWrapper(replicaCharacter);
+
+            const digit: number = subject.compare(new ReplicaCharacterWrapper(comparable));
+
+            expect(digit).toEqual(1);
+        });
+
+        test("should be less than with common prefix", () => {
+            replicaCharacter.positions = [
+                PositionIdentifier.create({digit: 1}),
+                PositionIdentifier.create({digit: 2})
+            ];
+            comparable.positions = [
+                PositionIdentifier.create({digit: 1}),
+                PositionIdentifier.create({digit: 2}),
+                PositionIdentifier.create({digit: 3})
+            ];
+            subject = new ReplicaCharacterWrapper(replicaCharacter);
+
+            const digit: number = subject.compare(new ReplicaCharacterWrapper(comparable));
+
+            expect(digit).toEqual(-1);
+        });
+
+        test("should be greater than with higher siteId", () => {
+            replicaCharacter.positions = [
+                PositionIdentifier.create({digit: 1, siteId: "b77d409a"})
+            ];
+            comparable.positions = [
+                PositionIdentifier.create({digit: 1, siteId: "b67d409a"})
+            ];
+            subject = new ReplicaCharacterWrapper(replicaCharacter);
+
+            const digit: number = subject.compare(new ReplicaCharacterWrapper(comparable));
+
+            expect(digit).toEqual(1);
+        });
+
+        test("should be less than with lower siteId", () => {
+            replicaCharacter.positions = [
+                PositionIdentifier.create({digit: 1, siteId: "b67d409a"})
+            ];
+            comparable.positions = [
+                PositionIdentifier.create({digit: 1, siteId: "b77d409a"})
+            ];
+            subject = new ReplicaCharacterWrapper(replicaCharacter);
+
+            const digit: number = subject.compare(new ReplicaCharacterWrapper(comparable));
+
+            expect(digit).toEqual(-1);
+        });
+
+        test("should be equal", () => {
+            replicaCharacter.positions = [
+                PositionIdentifier.create({digit: 1, siteId: "b77d409a"})
+            ];
+            comparable.positions = [
+                PositionIdentifier.create({digit: 1, siteId: "b77d409a"})
+            ];
+            subject = new ReplicaCharacterWrapper(replicaCharacter);
+
+            const digit: number = subject.compare(new ReplicaCharacterWrapper(comparable));
+
+            expect(digit).toEqual(0);
+        });
     });
 });
